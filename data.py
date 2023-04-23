67c666f1881932abc157bc156b7c6503476fcb17
Florian <>
2023-04-23 15:27:31 +0200

    added data loading function

---
+++ data.py
@@ -0,0 +1,23 @@
+
+def load_onto_notes(path):
+    sentences = []
+    with open(path) as file:
+        sentence = []
+        for line in file:
+            vals = line.strip().split("\t")
+            if len(vals)==1:
+                sentences.append(sentence)
+                sentence = []
+            else:
+                assert(len(vals)==2)
+                token, pos = vals
+                sentence.append(tuple(vals))
+    if len(sentence)>0:
+        sentences.append(sentence)
+    return sentences
+
+
+
+
+res = load_onto_notes("../pos_tagger_data/train.col")
+print(res)
