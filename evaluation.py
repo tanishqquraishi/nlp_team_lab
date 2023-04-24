80049f8af6e119afd96d29c00ca4b53a512461b3
Florian <>
2023-04-24 13:41:29 +0200

    changed evaluation methods to oop

--- evaluation.py
+++ evaluation.py
@@ -1,105 +1,136 @@
-from data import load_onto_notes
-
-def confusions(gold, pred: list) -> dict:
+class ConfusionMatrix(object):
     """
-    Creates a confusion matrix with a list of gold and predicted labels.
+    An evaluation class.
+    Can be used to see what kind of errors are made.
     """
-    conf = dict()       # Empty dict to store true positives (TP), false positives (FP), false negatives (FN) for each label
-    for g,p in zip(gold, pred):
-        if g not in conf:   
-            conf[g] = {"TP":0, "FP":0, "FN":0,}
-        if p not in conf:
-            conf[p] = {"TP":0, "FP":0, "FN":0,}
-        if g==p:        # If gold and pred match, increment TP count
-            conf[g]["TP"] += 1
-        else:           # If gold and pred mismatch, increment FN and FP counts
-            conf[g]["FN"] += 1
-            conf[p]["FP"] += 1
-    return conf
-
-def f1_score_micro(confusions: dict) -> dict:
-    """
-    Computes the micro-averaged F1 score using counts of true positives (TP), false positives (FP)
-    and false negatives (FN) for each label.
-    """
+    def __init__(self, confusions, all_tags, handle_nan=None):
+        """
+        Accepts a nested dictinary of confusiuons and a list of all labels.
+        Set 'handle_nan' to control what happens if an evalution metric 
+        is undefined (None: returns NaN, otherwise returns 'handle_nan').
+        """
+        self.confusions = confusions
+        self.all_tags = all_tags
+        self.nan = float("NaN") if handle_nan is None else handle_nan
     
-    TP, FP, FN = 0,0,0
-    for k,v in confusions.items():
-        TP += v["TP"]       # Increment TP with value of TP in dict
-        FP += v["FP"]       # Increment FP with value of TP in dict
-        FN += v["FN"]       # Increment FN with value of TP in dict
-    P = TP / (TP+FP) if (TP+FP) > 0 else float("NaN")   # Computes precision
-    R = TP / (TP+FN) if (TP+FN) > 0 else float("NaN")   # Computes recall 
-    F1 = 2*P*R / (P+R)
-    return {"P":P, "R":R, "F1":F1}
-
-def f1_score_macro(confusions: dict) -> dict:
-    """
-    Computes the macro F1 score using precision, recall and F1 scores.
-    """
-    
-    p_macro = 0     # Initialize macro precision
-    r_macro = 0     # Initialize macro recall
-    f1_macro = 0    # Initialize macro F1
-    for v in f1_score(confusions).values():
-        p_macro+=v["P"]
-        r_macro+=v["R"]
-        f1_macro+=v["F1"]
-    p_macro /= len(confusions)  
-    r_macro /= len(confusions)
-    f1_macro /= len(confusions)
-    return {"P":p_macro,"R":r_macro,"F1":f1_macro,}
-
-
-def f1_score(confusions: dict) -> dict:
-    """
-    Computes F1 score for each class i.e. each part of speech.
-    """
-    
-    f1_scores = {}
-    for k in confusions.keys():
-        d = confusions[k]["TP"]+confusions[k]["FP"]         # Computes the denominator
-        p = confusions[k]["TP"]/d if d>0 else float("NaN")  # Computes precision, returns NaN if denominator is 0
+    def errors2scores(self, errors):
+        """
+        Converts errors to evaluaton scores.
+        """
+        d = (errors["TP"] + errors["FP"])
+        P = (errors["TP"] / d ) if d!= 0 else self.nan
         #
-        d = confusions[k]["TP"]+confusions[k]["FN"]
-        r = confusions[k]["TP"]/d if d>0 else float("NaN")  # Computes recall, returns NaN if denominator is 0
+        d = (errors["TP"] + errors["FN"])
+        R = (errors["TP"] / d) if d!= 0 else self.nan
+        #
+        d = (P+R)
+        F1 = (2*P*R / d) if d!=0 else self.nan
+        
+        return {"P":P, "R":R, "F1":F1}
+    
+    @staticmethod
+    def from_data(gold, pred):
+        """
+        Create a new confusion matrix from two lists of labels.
+        """
+        # Count confusions and collect all used tags
+        conf = dict()
+        all_tags = set()
+        for g,p in zip(gold,pred):
+            if g not in conf:
+                conf[g] = dict()
+            if p not in conf[g]:
+                conf[g][p] = 0
+            conf[g][p] += 1
+            all_tags.update([g,p])
+        # Add missing zero-entries 
+        # (eg. if a tag occures only in gold or in only prediction)
+        for t in all_tags:
+            if t not in conf:
+                conf[t] = {k:0 for k in all_tags}
+            else:
+                for k in all_tags:
+                    if k not in conf[t]:
+                        conf[t][k] = 0
         ##
-        d = p+r
-        f1 = 2*p*r/d
-        f1_scores[k] = {"P":p, "R":r, "F1":f1}
-    return f1_scores
+        return ConfusionMatrix(conf, all_tags)
+    
+    def get_confusions(self, tag):
+        """
+        Returns how often 'tag' was confused with another tag.
+        """
+        # copy dict !!!
+        confs = dict(self.confusions[tag])
+        return confs
+    
+    def errors(self, tag):
+        """
+        Returns the number of errors for 'tag'.
+        """
+        TP = self.confusions[tag][tag]
+        FP = sum([self.confusions[tag][k] for k in self.all_tags if k!=tag])
+        FN = sum([self.confusions[k][tag] for k in self.all_tags if k!=tag])
+        return {"TP":TP, "FP":FP, "FN":FN}
+    
+    def f1_scores(self):
+        """
+        Returns all scores for each label.
+        """
+        scores = dict()
+        for tag in self.confusions:
+            err = self.errors(tag)
+            scores[tag] = self.errors2scores(err)
+        return scores
+    
+    def micro_f1(self):
+        """
+        Returns a micro averaged scores.
+        """
+        # first collect errors for all tags
+        err = {"TP":0, "FP":0, "FN":0}
+        for tag in self.confusions:
+            err_ = self.errors(tag)
+            err["TP"] += err_["TP"]
+            err["FP"] += err_["FP"]
+            err["FN"] += err_["FN"]
+        # calculate evalution scores from errors
+        return self.errors2scores(err)
+    
+    def macro_f1(self):
+        """
+        Returns macro averaged scores.
+        """
+        # Calculte scores for each tag
+        scores = {"P":0, "R":0, "F1":0}
+        for tag in self.confusions:
+            err = self.errors(tag)
+            scores_ = self.errors2scores(err)
+            scores["P"]  += scores_["P"] 
+            scores["R"]  += scores_["R"] 
+            scores["F1"] += scores_["F1"]
+        # and average them later on
+        scores["P"] /= len(self.confusions)
+        scores["R"] /= len(self.confusions)
+        scores["F1"] /= len(self.confusions)
+        return scores
 
 
-a = load_onto_notes("../pos_tagger_data/train.col")
-_,a = zip(*a[0])
-
-print("MACRO:")
-print(f1_score_macro(confusions(a,a)))
-print("MICRO:")
-print(f1_score_micro(confusions(a,a)))
-print("No Avg.:")
-print(f1_score(confusions(a,a)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    """
+    Testing goes here
+    """
+    a = ["A", "B", "C", "A", "B", "C", "A", "B", "C", "E"]
+    b = ["A", "A", "A", "B", "B", "B", "C", "C", "C", "C"]
+    confmat = ConfusionMatrix.from_data(a, b)
+    print("Confusions of 'A':")
+    print(confmat.get_confusions('A'))
+    print("Confusions of 'C':")
+    print(confmat.get_confusions('C'))
+    print("Errors on 'A':")
+    print(confmat.errors('A'))
+    print("MACRO:")
+    print(confmat.macro_f1())
+    print("MICRO:")
+    print(confmat.micro_f1())
+    print("No Avg.:")
+    print(confmat.f1_scores())
